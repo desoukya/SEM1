d751ed9e2cb44e44c01c99172ea070611287e498
Amr Desouky <desoukya@gmail.com>
2016-03-20 23:22:31 +0200

    datepicker-popup

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -6,6 +6,30 @@ App.controller('mainCtrl', function($scope, FlightsSrv, $location) {
     /* Initialize Scope Variables */
     $scope.selectedOrigin = undefined;
 
+    $scope.open1 = function() {
+      $scope.popup1.opened = true;
+    };
+
+    $scope.open2 = function() {
+      $scope.popup2.opened = true;
+    };
+
+    $scope.setDate = function(year, month, day) {
+      $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.popup1 = {
+      opened: false
+    };
+
+    $scope.popup2 = {
+      opened: false
+    };    
+
     /* Retrieve List of Oirign Airports */
     function originAirports() {
       FlightsSrv.getOriginAirports().success(function(airports) {
